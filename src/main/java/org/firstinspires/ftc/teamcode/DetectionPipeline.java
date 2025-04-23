485dc60e4bdf6bc1cd556ab05b9eefff0c3e3e80
Shrey Karmacharya <shreykarmacharya@gmail.com>
2025-04-23 07:42:21 -1000

    Implemented threshold, find contour algorithm

--- src/main/java/org/firstinspires/ftc/teamcode/DetectionPipeline.java
+++ src/main/java/org/firstinspires/ftc/teamcode/DetectionPipeline.java
@@ -3,8 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.opencv.core.Core;
 import org.opencv.imgproc.Imgproc;
@@ -13,8 +17,8 @@ public class DetectionPipeline extends OpenCvPipeline {
     Mat hsv = new Mat();
 
     // Thresholding
-    Scalar hsvLowerThreshold = new Scalar(80, 68, 0);
-    Scalar hsvUpperThreshold = new Scalar(120, 255, 150);
+    Scalar hsvLowerThreshold = new Scalar(62, 67, 0);
+    Scalar hsvUpperThreshold = new Scalar(155, 255, 150);
     Mat thresholded = new Mat();
     Mat thresholdedRGB = new Mat();
 
@@ -22,9 +26,9 @@ public class DetectionPipeline extends OpenCvPipeline {
     Mat masked = new Mat();
     Mat maskedRGB = new Mat();
 
-    // Bounding box
-    int[] bottomLeft;
-    int[] topRight;
+    // Contours
+    List<MatOfPoint> contours = new ArrayList<>();
+    Mat hierarchy = new Mat(); 
 
     // Output mat (with color labeling, object detection)
     Mat output = new Mat();
@@ -40,26 +44,22 @@ public class DetectionPipeline extends OpenCvPipeline {
         // Mask original pixel values over only the thresholded values (display/debug only)
         Imgproc.cvtColor(thresholded, thresholdedRGB, Imgproc.COLOR_GRAY2RGBA);
         Core.bitwise_and(input, thresholdedRGB, masked);
+        
+        output = input;
 
-        // Find bounding box
-        // for (int i = 0; i < thresholded.rows(); i++) {
-        //     for (int j = 0; j < thresholded.cols(); j++) {
-        //         // If the pixel in thresholded
-        //         if (thresholded.get(i, j)[0] == 255) {
-        //             if (bottomLeft == null) {
-        //                 bottomLeft[0] = i;
-        //                 bottomLeft[1] = j;
-        //             }
-        //             if (topRight == null) {
-        //                 topRight[0] = i;
-        //                 topRight[1] = j;
-        //             }
-        //         }
-        //     }
-        // }
+        // Find contours
+        Imgproc.findContours(thresholded, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        return masked;
-        // return output;
+        // Draw contours
+        for (int i = 0; i < contours.size(); i++) {
+            Imgproc.drawContours(output, contours, i, new Scalar(0, 255, 0));
+        }
+
+        // Get bounding box
+
+        // return masked;
+        // return thresholded;
+        return output;
     }
 
 }
