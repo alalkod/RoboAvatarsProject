c81d9655d9c5c909270743b50401cdd85f1bc184
Shrey Karmacharya <shreykarmacharya@gmail.com>
2025-04-23 12:48:28 -1000

    Canny edge detection

--- src/main/java/org/firstinspires/ftc/teamcode/DetectionPipeline.java
+++ src/main/java/org/firstinspires/ftc/teamcode/DetectionPipeline.java
@@ -1,54 +1,68 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
 import org.opencv.core.Scalar;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.opencv.core.Core;
+
 import org.opencv.imgproc.Imgproc;
 
 public class DetectionPipeline extends OpenCvPipeline {
     Mat hsv = new Mat();
 
     // Thresholding
-    Scalar hsvLowerThreshold = new Scalar(62, 67, 0);
-    Scalar hsvUpperThreshold = new Scalar(155, 255, 150);
+    //                                           degrees, percent, percent
+    public Scalar hsvLowerThreshold = new Scalar(118, 34, 0);
+    public Scalar hsvUpperThreshold = new Scalar(280, 100, 59);
+
+    // Convert to 2 degrees, 0-255, 0-255
+    Scalar hsvTrueLowerThreshold = new Scalar(hsvLowerThreshold.val[0] / 2, hsvLowerThreshold.val[1] * 255 / 100, hsvLowerThreshold.val[2] * 255 / 100);
+    Scalar hsvTrueUpperThreshold = new Scalar(hsvUpperThreshold.val[0] / 2, hsvUpperThreshold.val[1] * 255 / 100, hsvUpperThreshold.val[2] * 255 / 100);
     Mat thresholded = new Mat();
     Mat thresholdedRGB = new Mat();
 
     // Masking
     Mat masked = new Mat();
-    Mat maskedRGB = new Mat();
+
+    // Edge detection
+    public double cannyLowerThreshold = 50f;
+    public double cannyUpperThreshold = 60f;
+
+    Mat cannyEdge = new Mat();
 
     // Contours
     List<MatOfPoint> contours = new ArrayList<>();
     Mat hierarchy = new Mat(); 
+    public int Ncontours;
 
     // Output mat (with color labeling, object detection)
     Mat output = new Mat();
 
     @Override
     public Mat processFrame(Mat input) {
+        output = input;
+
         // Convert to hsv
         Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
         
         // HSV threshold
-        Core.inRange(hsv, hsvLowerThreshold, hsvUpperThreshold, thresholded);
+        Core.inRange(hsv, hsvTrueLowerThreshold, hsvTrueUpperThreshold, thresholded);
 
         // Mask original pixel values over only the thresholded values (display/debug only)
         Imgproc.cvtColor(thresholded, thresholdedRGB, Imgproc.COLOR_GRAY2RGBA);
         Core.bitwise_and(input, thresholdedRGB, masked);
-        
-        output = input;
+
+        // Canny edge detector
+        Imgproc.Canny(masked, cannyEdge, cannyLowerThreshold, cannyUpperThreshold);
 
         // Find contours
-        Imgproc.findContours(thresholded, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(cannyEdge, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        Ncontours = contours.size();
 
         // Draw contours
         for (int i = 0; i < contours.size(); i++) {
@@ -57,8 +71,9 @@ public class DetectionPipeline extends OpenCvPipeline {
 
         // Get bounding box
 
-        // return masked;
         // return thresholded;
+        // return masked;
+        // return cannyEdge;
         return output;
     }
 
